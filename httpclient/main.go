768cc2d2bc0ef4805281037b1f5b29c93b638d44
pell <52250856+kakui0621@users.noreply.github.com>
2023-08-19 21:14:18 +0900

    Create main.go

---
+++ httpclient/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: httpclient <url>")
+		return
+	}
+
+	url := os.Args[1]
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching URL:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
